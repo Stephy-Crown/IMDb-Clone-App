8ff1811077142a0812e70ecc3352e2d37400bce8
Stephanie <udemezuestephanie6@gmail.com>
2023-09-30 14:29:12 +0100

    fetch data from TMDB api to the homepage

--- src/app/components/Results.jsx
+++ src/app/components/Results.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Results({ results }) {
-  console.log(results);
+  // console.log(results);
   return (
     <div>
       {results.map((result) => (
